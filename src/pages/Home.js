2fbee181bf3b04dad85006277a2db0b090d0e100
Nate Worley <immythz@gmail.com>
2022-05-19 17:29:00 -0500

    removed console logs

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React, { useState } from "react";
 import NavBar from "../components/Navbar/NavBar";
 import Carousel from "../components/Carousel/Carousel";
 import Footer from "../components/Footer/Footer";
@@ -6,32 +6,21 @@ import InfoSection from "../components/InfoSection/InfoSection";
 
 import InfoSet from "../InfoSet.json";
 
-// import test from "../images/1st_Date.JPG";
-// import test2 from "../images/6-month-Legacy West.jpg";
-// import test3 from "../images/1-yr-Bluebonnets.jpg";
-// import test4 from "../images/NA-Background.png";
-
 export default function Home(props) {
     const [count, setCount] = useState(0);
     const [images, setImages] = useState(InfoSet.images);
-
-    useEffect(() => {
-        console.log(images);
-    }, []);
 
     let total = images.length;
 
     const nextImage = () => {
         if (count <= total - 2) {
             setCount(count + 1);
-            console.log(count);
         } 
     }
 
     const prevImage = () => {
         if (count > 0) {
             setCount(count - 1);
-            console.log(count);
         } 
     }
     return (
