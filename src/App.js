1702ee63da55d63d8e48e4de35cc81e7e9239a2a
Nate Worley <immythz@gmail.com>
2022-05-19 21:51:33 -0500

    removed routing

--- src/App.js
+++ src/App.js
@@ -1,21 +1,11 @@
 import React from 'react'
-import {
-  BrowserRouter as Router,
-  Route, 
-  Routes,
-} from 'react-router-dom'
 import Home from "./pages/Home"
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <Router>
-        <Routes>
-          <Route exact path='/' element={<Home/>} />
-          <Route exact path='/home' element={<Home />} />
-        </Routes>
-      </Router>
+      <Home />
     </div>
   );
 }
